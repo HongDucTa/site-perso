9853a81cd52565cf2b69ef018eb926555b30afc2
Hông Duc TA <hong-duc.ta@protonmail.com>
2019-03-04 22:48:14 +0100

    feat: added basic layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormationComponent } from './formation/formation.component';
+
+import * as $ from 'jquery';
+import { cleanSession } from 'selenium-webdriver/safari';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mon site';
+
+  Clean(): void
+  {
+    $("app-home").hide();
+    $("app-formation").hide();
+    $("app-projets").hide();
+    $("app-contact").hide();
+  }
+
+  Home(): void
+  {
+    this.Clean();
+    $("app-home").show();
+  }
+
+  Formation(): void
+  {
+    this.Clean();
+    $("app-formation").show();
+  }
+
+  Projets(): void
+  {
+    this.Clean();
+    $("app-projets").show();
+  }
+
+  Contact(): void
+  {
+    this.Clean();
+    $("app-contact").show();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,19 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FormationComponent } from './formation/formation.component';
+import { ProjetsComponent } from './projets/projets.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    FormationComponent,
+    ProjetsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
